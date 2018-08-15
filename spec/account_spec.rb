bbc0954cd3e50bda1e15f5b2ed5902e4ad68e156
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-08-15 10:04:13 +0100

    add #balance, .deposit, and .withdraw

---
+++ spec/account_spec.rb
@@ -0,0 +1,25 @@
+require 'account'
+
+describe Account do 
+
+  describe '.deposit' do
+    it { is_expected.to respond_to(:deposit).with(1).argument }
+
+    it "should increase #balance by amount" do 
+      expect { subject.deposit(50) }.to change { subject.balance }.by (+50) 
+    end
+
+  end
+
+  describe ".withdraw" do
+    it { is_expected.to respond_to(:withdraw).with(1).argument }
+
+    it "should decrease #balance by amount" do 
+      expect { subject.withdraw(10) }.to change { subject.balance }.by (-10)
+    end
+  end
+end
+
+describe Printer do 
+  
+end
